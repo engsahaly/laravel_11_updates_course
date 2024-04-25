f713eb405584769f2ccb5b9610121af2724f1dd0
engsahaly <engsahaly@gmail.com>
2024-04-25 23:02:45 +0200

    middleware changes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Middleware\AuthMiddleware;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Application;
+use App\Http\Middleware\TestMiddleware;
+use App\Http\Middleware\Test2Middleware;
+use App\Http\Middleware\Test3Middleware;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
@@ -13,7 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__ . '/../routes/console.php',
         channels: __DIR__ . '/../routes/channels.php',
         health: '/up',
-        using: function () {
+        then: function () {
             Route::prefix('admin')->middleware('web')
                 ->name('admin.')
                 ->group(base_path('routes/admin.php'));
@@ -27,7 +31,27 @@ return Application::configure(basePath: dirname(__DIR__))
         }
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        // $middleware->prepend([
+        //     TestMiddleware::class,
+        // ]);
+        // \Illuminate\Foundation\Http\Middleware\TrimStrings::class,
+
+        // $middleware->appendToGroup('testGroup', [
+        //     TestMiddleware::class,
+        //     Test2Middleware::class,
+        // ]);
+        // $middleware->prependToGroup('testGroup', [
+        //     Test3Middleware::class,
+        // ]);
+
+        // $middleware->web(append: [
+        //     TestMiddleware::class,
+        // ]);
+
+        $middleware->alias([
+            'test' => TestMiddleware::class,
+            'auth2' => AuthMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
