4ba99356a41fad7e63f98e06bed7c0d6bea732ad
engsahaly <engsahaly@gmail.com>
2024-04-25 16:42:48 +0200

    new make commands

---
+++ app/Examples/Example2Class.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Examples;
+
+class Example2Class
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Invoke the class instance.
+     */
+    public function __invoke(): void
+    {
+
+    }
+}
---
+++ app/Examples/ExampleClass.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Examples;
+
+class ExampleClass
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
